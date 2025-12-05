b81f46d59f350828b6b06ab025b592be3f07bfec
Lisa Baek <110780435+seolibek@users.noreply.github.com>
2025-12-05 18:02:19 +0000

    3x tests

--- benchmarks/sbaek25-test1.lisp
+++ benchmarks/sbaek25-test1.lisp
@@ -10,38 +10,19 @@
   (+ a (+ b c))
 )
 
-(define (sum4 a b c d)
-  (+ a (+ b (+ c d)))
-)
-
 (define (triple x)
   (+ x (+ x x))
 )
 
-(define (quad x)
-  (+ x (+ x (+ x x))) 
-)
-
 (define (combine a b)
-  (+ (triple a) (inc b))    
-)
-
-(define (mix a b c)
-  (sum4 (combine a b) (triple c) (dec a) (dec b))
+  (+ (triple a) (inc b))
 )
 
 (define (compute a b c)
-  (sum3 (combine a b) (quad c) (mix a b c))
+  (sum3 (combine a b) (triple c) (dec a))
 )
 
-(define (total a b c)
-  (sum4 (compute a b c)
-        (combine (inc a) (dec b))
-        (triple (quad c))
-        (inc (mix a b c)))
-)
-
-(let ((x (total 2 3 4)))
+(let ((x (compute 2 3 4)))
   (if (zero? 0)     
       (print x)
       (print (inc x))
